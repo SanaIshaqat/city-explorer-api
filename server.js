a0955f0254e3dc9d788d964641ba348f62baaf04
SanaIshaqat <sanaishaqat@gmail.com>
2021-09-15 16:09:11 +0300

    AllFixedLab08

--- server.js
+++ server.js
@@ -68,29 +68,35 @@ class WeekForCast {
 
 }
 
-// let handleMovie = async (req, res) => {   
-//     let country=req.query.production_countries;
-    
-//     let url = `https://api.themoviedb.org/3/movie/550?api_key=${process.env.REACT_APP_THEMOVIEDB_API_KEY}&production_countries=${country}`
-//     let responseMovies = await axios.get(url);
-//     let movieData = responseMovies.data;
-//     let finalCleanedMovies = movieData.map(item => {
-//         return new MoviesCountry(item.origin_country, item.title);
-//     })
-//     console.log(finalCleanedMovies);
-//     res.status(200).json(movieData.data);
-// }
+let handleMovie = async (req, res) => {   
+    let country=req.query.query;
 
-// app.get('/movies', handleMovie)
+    let url=`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_THEMOVIEDB_API_KEY}&query=${country}`
+  
+    let responseMovies = await axios.get(url);
+    let movieData = responseMovies.data;
+    console.log(movieData)
+    let finalCleanedMovies = movieData.results.map(item => {
+        return new MoviesCountry(item.title,item.overview, item.vote_average,item.vote_count,item.poster_path,item.popularity,item.release_date);
+    })
+    console.log('Test',finalCleanedMovies);
+    res.status(200).json(finalCleanedMovies);
+}
+
+app.get('/movies', handleMovie)
 
 
-
-// class MoviesCountry{
-//     constructor(origin,title){
-//         this.title=title,
-//         this.origin=origin
-//     }
-// }
+class MoviesCountry{
+    constructor(title,overview,vote_average,vote_count,poster_path,popularity,release_date){
+        this.title=title,
+        this.overview=overview, 
+        this.vote_average=vote_average,
+        this.vote_count=vote_count,
+        this.poster_path=`https://image.tmdb.org/t/p/w500${poster_path}`,
+        this.popularity=popularity,
+        this.release_date=release_date
+    }
+}
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 
